f55f58508cd54c4a4d92f0c115064385a687bbeb
Duyb <1255248666@qq.com>
2021-11-30 17:44:43 +0800

    feat: 进度条 nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './router';
+import nprogress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+// 进度条的配置
+nprogress.configure({});
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// VueRouter 4 中可以不写 next 了，默认就是通过状态
+router.beforeEach((to, from) => {
+  nprogress.start();
+});
+
+router.afterEach(() => {
+  nprogress.done();
 });
 
 export default router;
