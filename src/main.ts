44777778ac7ce5d99600626840dbd686cd1ee290
Duyb <1255248666@qq.com>
2021-12-02 16:24:48 +0800

    feat: 自动注册全局组件

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,14 @@ import { store, key } from './store';
 import './styles/index.scss';
 import ElementPlus from './plugins/element-plus';
 
-createApp(App).use(store, key).use(router).use(ElementPlus).mount('#app');
+const app = createApp(App);
+
+app.use(store, key).use(router).use(ElementPlus);
+
+// 自动注册全局组件
+const modules = import.meta.globEager('./components/**/index.ts');
+for (const path in modules) {
+  app.use(modules[path].default);
+}
+
+app.mount('#app');
