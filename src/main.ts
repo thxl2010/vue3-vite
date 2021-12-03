a827ccf0ebb7a06c94ed94b125bcd626f802f32c
Duyb <1255248666@qq.com>
2021-12-03 15:28:55 +0800

    feat: 权限管理/规则 & vxe-table

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import router from './router';
 import { store, key } from './store';
 import './styles/index.scss';
 import ElementPlus from './plugins/element-plus';
+import 'xe-utils';
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
 
 const app = createApp(App);
 
-app.use(store, key).use(router).use(ElementPlus);
+app.use(store, key).use(router).use(ElementPlus).use(VXETable);
 
 // 自动注册全局组件
 const modules = import.meta.globEager('./components/**/index.ts');
