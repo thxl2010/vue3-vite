1879e37b9c20f0376f95ea6f522eca9c38c8f750
Duyb <1255248666@qq.com>
2021-12-05 22:34:24 +0800

    feat: add product : wangeditor & drag - sortablejs

--- src/components/TextEditor/index.vue
+++ src/components/TextEditor/index.vue
@@ -3,47 +3,50 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, watch, ref } from '@vue/runtime-core'
-import E from 'wangeditor'
+import { onMounted, watch, ref } from '@vue/runtime-core';
+import E from 'wangeditor';
 
 const props = defineProps({
   modelValue: {
     type: String,
-    default: ''
-  }
-})
+    default: '',
+  },
+});
 
 interface EmitsType {
-  (e: 'update:model-value', value: string): void
+  (e: 'update:model-value', value: string): void;
 }
 
-const emit = defineEmits<EmitsType>()
+const emit = defineEmits<EmitsType>();
 
-const editor = ref<InstanceType<typeof E> | null>(null)
+const editor = ref<InstanceType<typeof E> | null>(null);
 
-const unWatchModelValue = watch(() => props.modelValue, () => {
-  // 操作 DOM 的方式修改内容
-  editor.value?.txt.html(props.modelValue)
-  unWatchModelValue() // 取消监视
-})
+const unWatchModelValue = watch(
+  () => props.modelValue,
+  () => {
+    // 操作 DOM 的方式修改内容
+    editor.value?.txt.html(props.modelValue);
+    unWatchModelValue(); // ! 取消监视
+  }
+);
 
 onMounted(() => {
-  initEditor()
-})
+  initEditor();
+});
 
 const initEditor = () => {
-  editor.value = new E('#editor')
+  editor.value = new E('#editor');
 
   // 配置 onchange 回调函数
   editor.value.config.onchange = function (newHtml: string) {
-    emit('update:model-value', newHtml)
-  }
+    emit('update:model-value', newHtml);
+  };
 
-  editor.value.config.zIndex = 500
+  editor.value.config.zIndex = 500;
 
-  editor.value.create()
+  editor.value.create();
   // editor.value.txt.html(props.modelValue) // 注意：必须在 create 之后
-}
+};
 </script>
 
 <style lang="scss" scoped></style>
