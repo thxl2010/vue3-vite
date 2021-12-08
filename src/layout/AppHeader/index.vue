098493ccc526f7c44fe406d68d0b5366f147a2f5
Duyb <1255248666@qq.com>
2021-12-08 15:10:36 +0800

    fix: component name

--- src/layout/AppHeader/index.vue
+++ src/layout/AppHeader/index.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts" setup>
-import ToggleSidebar from './ToggleSidebar.vue';
+import ToggleSidebar from './ToggleSideBar.vue';
 import Breadcrumb from './Breadcrumb.vue';
 import FullScreen from './FullScreen.vue';
 import UserInfo from './UserInfo.vue';
